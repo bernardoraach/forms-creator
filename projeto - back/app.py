fbcaa6991d658a64d4513ebb6a8154e64a48614e
bernardoraach <bernardo.rach@gmail.com>
2026-06-17 11:59:59 -0300

    update hosting

--- projeto - back/app.py
+++ projeto - back/app.py
@@ -83,5 +83,5 @@ def api_gerar_documento():
         }), 400
 
 if __name__ == '__main__':
-    port = int (os.environ.get("PORT", 5001))
+    porta = int (os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=porta, debug=False)  # pyright: ignore[reportUndefinedVariable]
